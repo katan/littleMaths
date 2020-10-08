d40b7c142b549ae0d84708dafed3f1d70c9b0824
Ricard Catalán <zicard@gmail.com>
2020-10-08 20:34:35 +0200

    Implement vertical view

--- src/app/shared/show-question/show-question.component.ts
+++ src/app/shared/show-question/show-question.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { LocalStorageService } from '@app/services/local-storage.service';
+import { stringify } from 'querystring';
+import { Operation } from '../constants';
 
 @Component({
   selector: 'app-show-question',
@@ -6,12 +9,31 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./show-question.component.scss'],
 })
 export class ShowQuestionComponent implements OnInit {
+  showVerticalView: boolean;
 
   @Input() question: string;
   @Input() answer: string;
+  @Input() operation: Operation;
 
-  constructor() { }
+  constructor(private localStorage: LocalStorageService) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.showVerticalView = this.localStorage.get('verticalOperationView');
+  }
 
+  generateVerticalView(): string {
+    const values = this.getValues();
+    let render = '';
+
+    for (const value of values) {
+      render += `<div class="value">${value}</div>`;
+    }
+
+    return render += `<div class="operator">${this.operation}</div>`;
+  }
+
+  getValues(): string[] {
+    const regExp = new RegExp(`[${Operation.summation},${Operation.subtraction},${Operation.multiplication},${Operation.division}]`);
+    return this.question.replace(/\s/g, '').split(regExp);
+  }
 }
